771567026192f41b885fb8eb0e04bbce33255498
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2018-10-30 00:18:51 -0300

    Add sink for %wins

--- src/games_summarier.py
+++ src/games_summarier.py
@@ -3,9 +3,11 @@ from multiprocessing import Process
 from score import AcummulatedScore
 import zmq
 class GamesSummarier(Process):
-    def __init__(self, incoming_address, incoming_port):
+    def __init__(self, incoming_address, incoming_port, outcoming_address, outcoming_port):
         self.incoming_address = incoming_address
         self.incoming_port = incoming_port
+        self.outcoming_address = outcoming_address
+        self.outcoming_port = outcoming_port
         super(GamesSummarier, self).__init__()
 
     def _init(self):
@@ -13,10 +15,16 @@ class GamesSummarier(Process):
         self.socket = self.context.socket(zmq.PULL)
         # We can connect to several endpoints if we desire, and receive from all.
         self.socket.connect('tcp://{}:{}'.format(self.incoming_address, self.incoming_port))
+        
+        self.socket2 = self.context.socket(zmq.PUSH)
+        # We can connect to several endpoints if we desire, and receive from all.
+        self.socket2.connect('tcp://{}:{}'.format(self.outcoming_address, self.outcoming_port))
 
     def _get_game(self):
         return self.socket.recv_string()
 
+    def _send_result(self, result):
+        self.socket2.send_string(result)
 
     def run(self):
         self._init()
@@ -35,11 +43,11 @@ class GamesSummarier(Process):
             
             score_string = self._get_game()
 
-
-        print('% wins home team:' + str(float(home_wins) / total_games))
-
+        self._send_result("{}%".format(float(home_wins) / total_games * 100 ))
+        self._send_result("END")
         self._close()
 
     def _close(self):
         self.socket.close()
+        self.socket2.close()
         self.context.term()
--- src/main.py
+++ src/main.py
@@ -3,12 +3,14 @@ from games_summarier import GamesSummarier
 from players_processer.players_processer import PlayersProcesser
 from points_summarier.points_summarier import PointsSummarier
 from reader import Reader
+from sink import Sink
 from time import sleep
 
 if __name__ == '__main__':
     incoming_address, incoming_port = '127.0.0.1', '2000'
     games_calculator = GamesCalculator(incoming_address, incoming_port, '127.0.0.1', '2001')
-    games_summarier = GamesSummarier('127.0.0.1', '2001')
+    games_summarier = GamesSummarier('127.0.0.1', '2001', '127.0.0.1', 2002)
+    sink = Sink('127.0.0.1', 2002, '%home-wins.txt')
     players_processer = PlayersProcesser(incoming_address, incoming_port)
     points_summarier2 = PointsSummarier(2, incoming_address, incoming_port)
     points_summarier3 = PointsSummarier(3, incoming_address, incoming_port)
@@ -21,6 +23,7 @@ if __name__ == '__main__':
     players_processer.start()
     points_summarier3.start()
     points_summarier2.start()
+    sink.start()
 
     sleep(1)
     reader.start()
@@ -32,7 +35,7 @@ if __name__ == '__main__':
     players_processer.join()
     points_summarier3.join()
     points_summarier2.join()
-
+    sink.join()
 
 
 
